ae2f269f5a781c9200a510dbbfe4faedf176fa8b
Vedant Vatare <vedantvatare73@gmail.com>
2023-11-07 16:58:36 +0530

    Necessary changes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let btn = document.querySelector("#btn");
-
+let op = document.getElementById("output");
 btn.addEventListener("click", function(){
-var from_num = document.querySelector("#FromOption");
-var to_num = document.querySelector("#toOption");
+let from_num = document.querySelector("#FromOption");
+let to_num = document.querySelector("#toOption");
     alert(from_num.value)
 })
+let steps = {
+    step1: function (){
+
+    }
+};
